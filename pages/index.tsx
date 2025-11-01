bc7a71ebd9a4aacd6addc144dadf947b9218f4da
hyein89 <bihontai@gmail.com>
2025-11-01 13:20:24 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+// pages/index.tsx
+import { useEffect } from "react";
 import Head from "next/head";
 
 export default function Home() {
+  const REDIRECT_URL = "https://bestsmartvpn.com/qyzhbbw1?key=94c0b7f8c03251e52b0a13aca62c58cd";
+
+  useEffect(() => {
+    window.location.href = REDIRECT_URL;
+  }, []);
+
   return (
     <>
       <Head>
-        <title>Home</title>
-        <meta name="description" content="Halaman utama" />
+        <meta httpEquiv="refresh" content={`0;URL=${REDIRECT_URL}`} />
       </Head>
-      <main style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
-        <h1>Welcome to Tes.vercel.app</h1>
-      </main>
     </>
   );
 }
