d61ad4cefff3efe0d8cf8954446f92a587ab7c5c
hyein89 <bihontai@gmail.com>
2025-11-01 13:23:45 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
-// pages/index.tsx
 import { useEffect } from "react";
 import Head from "next/head";
+import { REDIRECT_URL } from "../lib/config"; // sesuaikan path
 
 export default function Home() {
-  const REDIRECT_URL = "https://bestsmartvpn.com/qyzhbbw1?key=94c0b7f8c03251e52b0a13aca62c58cd";
-
   useEffect(() => {
     window.location.href = REDIRECT_URL;
   }, []);
